7e7c47b20c03dd2561e7d76860be48580507bd14
Jacqueline Phan <jtphan14@gmail.com>
2016-06-16 11:18:41 -0700

    Add working registration and login functionality

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -3,15 +3,27 @@ get '/users/new' do
 end
 
 post '/users' do
-  # redirect '/users'
+  @user = User.new(params[:user])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    status 422
+    @errors = @user.errors.full_messages
+    erb :'/users/new'
+  end
 end
 
 get '/users' do
 end
 
 get '/users/login' do
+  erb :'users/login'
 end
 
+post '/users/login' do
+  erb :'users/login'
+end
 delete '/users/logout' do
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,10 @@ include BCrypt
     self.password_hash = @password
   end
 
+  def self.authenticate(email, password)
+    user = User.find_by(email: email)
+    return user if user && user.password == password
+    nil
+  end
+
 end
