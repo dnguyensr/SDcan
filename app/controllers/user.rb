5fa79d091a48bc3ae8bd4ea392e2dbf5ba8ad36b
Jacqueline Phan <jtphan14@gmail.com>
2016-06-16 14:26:06 -0700

    Update nav bar to change depending on logged in status, update controller to handle registration and logging in and out

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -15,15 +15,36 @@ post '/users' do
 end
 
 get '/users' do
+  @users = User.all
+  erb :'users/index'
 end
+
+# get '/users/:id' do
+#   #can use current_user to handle this as well if we use users/show or something instead
+#   @user = User.find(params[:id])
+#   erb :'users/show'
+# end
+
 
 get '/users/login' do
   erb :'users/login'
 end
 
 post '/users/login' do
-  erb :'users/login'
+  @auth_user = User.authenticate(params[:user][:email], params[:user][:password])
+  if @auth_user
+    session[:user_id] = @auth_user.id
+    redirect '/'
+  else
+    @user = User.new
+    status 422
+    @errors = @user.errors.full_messages
+    erb :'users/login'
+  end
 end
-delete '/users/logout' do
+
+get '/users/logout' do
+  session[:user_id] = nil
+  redirect '/'
 end
 
