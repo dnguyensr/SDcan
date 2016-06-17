fc5c95422b9c84305e3ce83ffb9cc0704be54c31
Jacqueline Phan <jtphan14@gmail.com>
2016-06-16 18:42:13 -0700

    Add edit button for projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,5 +3,6 @@ class Project < ActiveRecord::Base
   belongs_to :organization
   has_many :volunteers
   validates :name, :begin_date, :end_date, presence: true
+  belongs_to :organizer , class_name: :User
   # custom validator for date
 end
