6878aca3e98fdda9f97d2540186add002fe82715
Jacqueline Phan <jtphan14@gmail.com>
2016-06-17 08:57:23 -0700

    Update blog and edit controller

--- app/controllers/stripe.rb
+++ app/controllers/stripe.rb
@@ -1,14 +1,3 @@
-
-
-
-# set :publishable_key, ENV['PUBLISHABLE_KEY']
-# set :secret_key, ENV['SECRET_KEY']
-
-# PUBLISHABLE_KEY=pk_test_6pRNASCoBOKtIshFeQd4XMUh SECRET_KEY=sk_test_BQokikJOvBiI2HlWgH4olfQ2
-# Stripe.api_key = settings.secret_key
-
-
-
 get '/charge' do
   erb :stripe
 end
